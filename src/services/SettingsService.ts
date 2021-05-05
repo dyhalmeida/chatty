40285a59ba052842432d4418ac7e088ddf23c013
Diego Almeida <dyhalmeida@gmail.com>
2021-05-05 16:15:11 -0300

    :sparkles: add day three | refactoring settings service

--- src/services/SettingsService.ts
+++ src/services/SettingsService.ts
@@ -1,4 +1,4 @@
-import { getCustomRepository } from 'typeorm';
+import { getCustomRepository, Repository } from 'typeorm';
 import { Setting } from '../entities/Setting';
 import { SettingsAlreadyExists } from '../errors/SettingsAlreadyExists';
 import { SettingsRepository } from '../repositories/SettingsRepository';
@@ -9,19 +9,24 @@ interface ISettingsService {
 }
 
 class SettingsService {
+  private settingsRepository: Repository<Setting>;
+
+  constructor() {
+    this.settingsRepository = getCustomRepository(SettingsRepository);
+  }
+
   async create({ username, chat }: ISettingsService): Promise<Setting> {
-    const settingsRepository = getCustomRepository(SettingsRepository);
-    const settingsAlreadyExists = await settingsRepository.findOne({
+    const settingsAlreadyExists = await this.settingsRepository.findOne({
       username,
     });
     if (settingsAlreadyExists) {
       throw new SettingsAlreadyExists('Configuração de usuário já existe');
     }
-    const settings = settingsRepository.create({
+    const settings = this.settingsRepository.create({
       username,
       chat,
     });
-    await settingsRepository.save(settings);
+    await this.settingsRepository.save(settings);
     return settings;
   }
 }
